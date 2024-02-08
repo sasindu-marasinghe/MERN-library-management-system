1bef1f81e52405c42bfc46d13face9f1748812a3
Sasindumarasinghe1221 <sasindumarasinghe74@gmail.com>
2024-02-08 18:44:35 +0530

    Server Configuration

---
+++ src/app.js
@@ -0,0 +1,19 @@
+
+import express from "express";
+import cors from "cors";
+import 'dotenv/config';
+
+const app = express();
+const PORT = process.env.PORT || "8090";
+
+app.use(cors());
+app.use(express.json({ limit: "20mb"}));
+
+app.get("/",(req,res, next) =>{
+    res.send("<h2> Library Management System API</H2>");
+    next();
+})
+
+app.listen(PORT, () => {
+    console.log(` Server is up and running on PORT ${PORT}`);
+});
